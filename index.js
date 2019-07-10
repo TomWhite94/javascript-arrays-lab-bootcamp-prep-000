2fb1fa0062e07a4e17bed77effe4065a3a2f06fb
Tom White <tomjameswhite94@gmail.com>
2019-07-10 10:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
+`function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  console.log(kittens)
+}
+destructivelyAppendKitten("Ralph")`
 
-function desructivelyPrependKitten(name) {
+
+`function desructivelyPrependKitten(name) {
   kittens.unshift(name)
   console.log(kittens)
 }
+desructivelyPrependKitten("Bob")`
